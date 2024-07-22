f50a0081e3cc2f4d1b64309c9136f9a12b15eead
Rboy <kh.reza10@gmail.com>
2024-07-22 08:08:26 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+#!/bin/python3
+
+#####################################
+#                                   #
+#    GitHub    : @therboy          #
+#    Developer : Reza Khodarahimi  #
+#  﫥  Copyright   2024              #
+#                                   #
+#####################################
 # main.py
 
 import tkinter as tk
