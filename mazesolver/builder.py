9741c1bfcea537e791399e16b3ed1080ed2a6a0e
Rboy <kh.reza10@gmail.com>
2024-07-22 08:07:19 +0330

    Update builder.py

--- mazesolver/builder.py
+++ mazesolver/builder.py
@@ -1,3 +1,12 @@
+#!/bin/python3
+
+#####################################
+#                                   #
+#    GitHub    : @therboy          #
+#    Developer : Reza Khodarahimi  #
+#  﫥  Copyright   2024              #
+#                                   #
+#####################################
 # mazesolver/builder.py
 
 def enable_maze_builder(gui):
