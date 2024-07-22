e9ca45f726e4af691b5765f659a7e00ef98f7ebd
Rboy <kh.reza10@gmail.com>
2024-07-22 08:06:57 +0330

    Update visualization.py

--- mazesolver/visualization.py
+++ mazesolver/visualization.py
@@ -1,3 +1,12 @@
+#!/bin/python3
+
+#####################################
+#                                   #
+#    GitHub    : @therboy          #
+#    Developer : Reza Khodarahimi  #
+#  﫥  Copyright   2024              #
+#                                   #
+#####################################
 # mazesolver/visualization.py
 
 import time
